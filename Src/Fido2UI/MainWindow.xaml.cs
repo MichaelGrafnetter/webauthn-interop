609023409801bb2fac8244c5737a8cfda7d6766e
Michael Grafnetter <michael.grafnetter@outlook.com>
2020-10-17 23:47:37 +0200

    Fix UI

--- Src/Fido2UI/MainWindow.xaml.cs
+++ Src/Fido2UI/MainWindow.xaml.cs
@@ -1,17 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Fido2NetLib;
+using Fido2NetLib.Objects;
 
 namespace DSInternals.Win32.WebAuthn.Fido2UI
 {
@@ -23,6 +14,66 @@ namespace DSInternals.Win32.WebAuthn.Fido2UI
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var config = new Fido2Configuration()
+            {
+                ChallengeSize = 32,
+                Origin = "login.microsoft.com",
+                ServerDomain = "login.microsoft.com",
+                ServerName = "Microsoft",
+                Timeout = 60000
+            };
+
+            var user = new Fido2User
+            {
+                Name = "michael.grafnetter@outlook.com",
+                DisplayName = "Michael Grafnetter",
+                Id = Base64Url.Decode("TUY65dH-Otl4jMdTRvlFQ1aApACYsuqGKSPQDQc1Bd4WVyw")
+            };
+
+            var authenticator = new AuthenticatorSelection
+            {
+                AuthenticatorAttachment = Fido2NetLib.Objects.AuthenticatorAttachment.CrossPlatform,
+                RequireResidentKey = true,
+                UserVerification = Fido2NetLib.Objects.UserVerificationRequirement.Required,
+            };
+
+            byte[] challenge = Encoding.ASCII.GetBytes("CbWTU93Ppbgok1glyka*K9sZSWkqpK3qS1ldeLJxsI4k3jMLIi3dl8VDx10siTGd8U5SNj8yyMIbqXQH!apXGnrhWmYlg2GNdEGddIkO03cql!kKVgKi*MqEIl9aPqmJdYuRMjrEYlIyzi4*wP0YSyA$");
+
+            var excludedCredentials = new List<PublicKeyCredentialDescriptor>()
+            {
+                new PublicKeyCredentialDescriptor(Base64Url.Decode("lz6_hw1jzaRNhhu9dt_M1Q=")),
+                new PublicKeyCredentialDescriptor(Base64Url.Decode("Zod6YhgNV2dQeT3v8ekjRpU0nVlEkPlpXF5Vx6f4P9g=")),
+                new PublicKeyCredentialDescriptor(Base64Url.Decode("sx2P4XkPO6TUoSf0pMEm3zi5gdwVrIRjiYvuTFRAkNMe_jVsntSgkyG5aV8er5GCA_G1X2idph-8lhhMFX3aaAyBCQIAAA="))
+            };
+
+            var extensions = new WinExtensionsIn()
+            {
+                HmacSecret = true,
+                CredProtect = UserVerification.Optional
+            };
+
+            var options = CredentialCreateOptions.Create(
+                config,
+                challenge,
+                user,
+                authenticator,
+                Fido2NetLib.Objects.AttestationConveyancePreference.Direct,
+                excludedCredentials,
+                extensions
+            );
+
+            options.PubKeyCredParams = new List<PubKeyCredParam>()
+            {
+                new PubKeyCredParam() { Alg = -7, Type = PublicKeyCredentialType.PublicKey },
+                new PubKeyCredParam() { Alg = -256, Type = PublicKeyCredentialType.PublicKey }
+            };
+
+            var webauthn = new WebAuthnApi();
+            var response = webauthn.AuthenticatorMakeCredential(options);
         }
     }
 }
