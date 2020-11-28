2d872fbb37a606485a432f9f6e3c15029d063f8b
Michael Grafnetter <michael.grafnetter@outlook.com>
2020-11-28 22:24:37 +0100

    Fixed sig path

--- Scripts/Set-BinAuthenticodeSignature.ps1
+++ Scripts/Set-BinAuthenticodeSignature.ps1
@@ -1,4 +1,4 @@
 ﻿$cer = Get-ChildItem -Path Cert:\CurrentUser\My -CodeSigningCert
-$target = 'C:\CQURE\Fido2Cmd\Fido2UI.exe'
+$target = "$PSScriptRoot\..\Build\bin\Release\Fido2UI\net472\Fido2UI.exe"
 $timeStamp = 'http://timestamp.comodoca.com'
-Set-AuthenticodeSignature -Certificate $cer -HashAlgorithm SHA256 -FilePath $target #-TimestampServer $timeStamp
+Set-AuthenticodeSignature -Certificate $cer -HashAlgorithm SHA256 -FilePath $target -TimestampServer $timeStamp
