399c765c4764218a64db233b0d02a57e803b1349
Michael Grafnetter <michael.grafnetter@outlook.com>
2024-08-13 06:32:19 +0200

    Fix sign and publish

--- Scripts/Build-PowerShellModule.ps1
+++ Scripts/Build-PowerShellModule.ps1
@@ -15,7 +15,7 @@ param(
 
 [string[]] $frameworks = @('net48', 'net6.0')
 [string] $repositoryRoot = Split-Path -Path $PSScriptRoot -Parent
-[string] $buildRoot = Join-Path -Path $repositoryRoot -ChildPath 'Build\bin'
+[string] $buildRoot = Join-Path -Path $repositoryRoot -ChildPath 'Build\bin\PSModule'
 [string] $moduleSourcePath = Join-Path -Path $repositoryRoot -ChildPath 'Src\DSInternals.Passkeys'
 [string] $libraryProject = Join-Path -Path $repositoryRoot -ChildPath 'Src\DSInternals.Win32.WebAuthn\DSInternals.Win32.WebAuthn.csproj'
 
@@ -24,12 +24,13 @@ foreach($currentConfiguration in $Configuration) {
     [string] $moduleDestinationPath = Join-Path -Path $configurationOutputPath -ChildPath 'DSInternals.Passkeys'
 
     # Copy module files (*.psd1, *.psm1,...)
+    New-Item -ItemType Directory -Path $moduleDestinationPath -Force | Out-Null
     Copy-Item -Path $moduleSourcePath -Destination $configurationOutputPath -Container -Recurse -Verbose -Force | Out-Null
 
     foreach($framework in $frameworks){
         [string] $frameworkSpecificPath = Join-Path -Path $moduleDestinationPath -ChildPath $framework
 
         # Copy the compiled binaries
-        dotnet.exe publish $libraryProject --output $frameworkSpecificPath --nologo --framework $framework --configuration $currentConfiguration --property:GenerateDocumentationFile=false --no-restore --no-build --self-contained false
+        dotnet.exe publish $libraryProject --output $frameworkSpecificPath --nologo --framework $framework --configuration $currentConfiguration --property:PublishDocumentationFile=false --no-restore --no-build --self-contained false
     }
 }
