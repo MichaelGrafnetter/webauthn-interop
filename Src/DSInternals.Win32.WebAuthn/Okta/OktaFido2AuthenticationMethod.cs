0432cc9de0be5cd739be45bbc9d59590f335ba31
Alex Seigler <alexseigler@hotmail.com>
2024-10-30 19:59:20 -0400

    Add summaries for properties

--- Src/DSInternals.Win32.WebAuthn/Okta/OktaFido2AuthenticationMethod.cs
+++ Src/DSInternals.Win32.WebAuthn/Okta/OktaFido2AuthenticationMethod.cs
@@ -25,21 +25,39 @@ namespace DSInternals.Win32.WebAuthn.Okta
             this.FulfillmentProvider = fulfillmentProvider;
         }
 
+        /// <summary>
+        /// ID for the Factor credential
+        /// </summary>
         [JsonPropertyName("credentialId")]
         public string CredentialId { get; }
 
+        /// <summary>
+        /// U2F appId string
+        /// </summary>
         [JsonPropertyName("appId")]
         public object AppId { get; }
 
+        /// <summary>
+        /// Undocumented
+        /// </summary>
         [JsonPropertyName("version")]
         public object Version { get; }
 
+        /// <summary>
+        /// Human-readable name of the authenticator
+        /// </summary>
         [JsonPropertyName("authenticatorName")]
         public string AuthenticatorName { get; }
 
+        /// <summary>
+        /// Undocumented
+        /// </summary>
         [JsonPropertyName("presetPinAvailable")]
         public object PresetPinAvailable { get; }
 
+        /// <summary>
+        /// Undocumented
+        /// </summary>
         [JsonPropertyName("fulfillmentProvider")]
         public object FulfillmentProvider { get; }
     }
@@ -77,28 +95,51 @@ namespace DSInternals.Win32.WebAuthn.Okta
 
             return JsonSerializer.Deserialize<OktaFido2AuthenticationMethod>(json);
         }
-
+        /// <summary>
+        /// ID of the Factor
+        /// </summary>
         [JsonPropertyName("id")]
         public string Id { get; }
 
+        /// <summary>
+        /// Type of Factor
+        /// </summary>
         [JsonPropertyName("factorType")]
         public string FactorType { get; }
 
+        /// <summary>
+        /// Provider for the Factor
+        /// </summary>
         [JsonPropertyName("provider")]
         public string Provider { get; }
 
+        /// <summary>
+        /// Name of the Factor vendor. This is usually the same as the provider except for On-Prem MFA where it depends on administrator settings.
+        /// </summary>
         [JsonPropertyName("vendorName")]
         public string VendorName { get; }
 
+        /// <summary>
+        /// Status of the Factor
+        /// </summary>
         [JsonPropertyName("status")]
         public string Status { get; }
 
+        /// <summary>
+        /// Timestamp when the Factor was enrolled
+        /// </summary>
         [JsonPropertyName("created")]
         public DateTime? Created { get; }
 
+        /// <summary>
+        /// Timestamp when the Factor was last updated
+        /// </summary>
         [JsonPropertyName("lastUpdated")]
         public DateTime? LastUpdated { get; }
 
+        /// <summary>
+        /// Specific attributes related to the Factor
+        /// </summary>
         [JsonPropertyName("profile")]
         public OktaProfile Profile { get; }
     }
